26f1e4a5c393122fbb7cf553606d88159a6c7e1b
Boris Jankovic <bjankovic998@gmail.com>
2024-06-26 09:54:33 +0200

    Add web

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,44 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let webview = vscode.commands.registerCommand(
+    "react-ext.namasteworld",
+    () => {
+      let panel = vscode.window.createWebviewPanel(
+        "webview",
+        "React",
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true,
+        }
+      );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "element-ai" is now active!');
+      let scriptSrc = panel.webview.asWebviewUri(
+        vscode.Uri.joinPath(context.extensionUri, "web", "dist", "index.js")
+      );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('element-ai.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from element-ai!');
-	});
+      let cssSrc = panel.webview.asWebviewUri(
+        vscode.Uri.joinPath(context.extensionUri, "web", "dist", "index.css")
+      );
 
-	context.subscriptions.push(disposable);
+      panel.webview.html = `<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <link rel="stylesheet" href="${cssSrc}" />
+          </head>
+          <body>
+            <noscript>You need to enable JavaScript to run this app.</noscript>
+            <div id="root"></div>
+            <script src="${scriptSrc}"></script>
+          </body>
+        </html>
+        `;
+    }
+  );
+
+  context.subscriptions.push(webview);
 }
 
 // This method is called when your extension is deactivated
