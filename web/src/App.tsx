63bd2b085d1379df030b4573b99ddfdaf5f0ae30
Boris Jankovic <bjankovic998@gmail.com>
2024-06-26 09:47:49 +0200

    Add web

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-gradient-to-r from-blue-600 to-purple-500 p-10">
+      <p className="text-white/80 text-xl font-semibold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, explicabo
+        doloremque deserunt, voluptates, fugiat dolorem consectetur odio autem
+        quas ipsa veniam ducimus necessitatibus exercitationem numquam assumenda
+        natus beatae sed velit!
+      </p>
+    </div>
+  );
+}
+
+export default App;
