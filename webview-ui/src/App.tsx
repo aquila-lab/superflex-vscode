eada56ab80e70729e03fefdcdf40cf1593f4f8d4
Boris Jankovic <bjankovic998@gmail.com>
2024-07-11 17:22:27 +0200

    Change how useEffect was defined

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -12,28 +12,26 @@ type View = 'chat' | 'login' | 'no_open_project';
 const App: React.FunctionComponent<{ vscodeAPI: VSCodeWrapper }> = ({ vscodeAPI }) => {
   const [view, setView] = useState<View>('login');
 
-  useEffect(
-    () =>
-      vscodeAPI.onMessage((message) => {
-        switch (message.command) {
-          case 'show_login_view':
-            setView('login');
-            break;
-          case 'show_chat_view':
-            setView('chat');
-            break;
-          case 'initialized':
-            if (!message.data) {
-              setView('no_open_project');
-              return;
-            }
+  useEffect(() => {
+    vscodeAPI.onMessage((message) => {
+      switch (message.command) {
+        case 'show_login_view':
+          setView('login');
+          break;
+        case 'show_chat_view':
+          setView('chat');
+          break;
+        case 'initialized':
+          if (!message.data) {
+            setView('no_open_project');
+            return;
+          }
 
-            setView('chat');
-            break;
-        }
-      }),
-    [view, vscodeAPI]
-  );
+          setView('chat');
+          break;
+      }
+    });
+  }, [vscodeAPI]);
 
   useEffect(() => {
     // Notify the extension host that we are ready to receive events
