2fa792564867be563906cf82fb28096e4925e2da
Boris Jankovic <bjankovic998@gmail.com>
2024-10-07 22:45:43 -0400

    Add auto scroll

--- webview-ui/src/components/chat/ChatMessageList.tsx
+++ webview-ui/src/components/chat/ChatMessageList.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React, { useEffect, useRef } from 'react';
 import { useAppSelector } from '../../core/store';
 import { LoadingDots } from '../ui/LoadingDots';
 import { ChatMessage } from './ChatMessage';
@@ -7,16 +6,29 @@ import { ChatMessage } from './ChatMessage';
 export const ChatMessageList: React.FC = () => {
   const messages = useAppSelector((state) => state.chat.messages);
   const isMessageProcessing = useAppSelector((state) => state.chat.isMessageProcessing);
+  const listRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    if (listRef.current) {
+      listRef.current.scrollTo({ top: listRef.current.scrollHeight, behavior: 'smooth' });
+    }
+  }, [messages]);
 
   return (
-    <div className="flex-1 flex flex-col justify-start gap-y-2 overflow-y-auto mb-4">
-      {messages.map((message) => (
-        <ChatMessage key={message.id} message={message} />
-      ))}
+    <div className="flex-1 flex flex-col gap-y-2 mb-4 overflow-hidden">
+      <ul ref={listRef} className="flex flex-col gap-y-2 overflow-y-auto">
+        {messages.map((message) => (
+          <li key={message.id}>
+            <ChatMessage message={message} />
+          </li>
+        ))}
+      </ul>
 
-      <div className="flex-initial mt-3 ml-2">
-        <LoadingDots isLoading={isMessageProcessing} />
-      </div>
+      {isMessageProcessing && (
+        <div className="flex-1 mt-3 ml-2">
+          <LoadingDots isLoading={true} />
+        </div>
+      )}
     </div>
   );
 };
