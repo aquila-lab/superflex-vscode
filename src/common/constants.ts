03bdc6ac82a1ea5a440a9460bf0066ffc0af64ef
Boris Jankovic <bjankovic998@gmail.com>
2024-10-09 13:30:39 -0400

    Add more analytics data

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,7 @@
+import { IS_PROD, APP_BASE_URL, API_BASE_URL, SUPERFLEX_POSTHOG_API_KEY } from "../../shared/common/constants";
+
+export { IS_PROD, APP_BASE_URL, API_BASE_URL, SUPERFLEX_POSTHOG_API_KEY };
+
 /**
  * @enum {string}
  */
@@ -7,9 +11,6 @@ export const ApiErrorTypes = {
   SESSION_EXPIRED: "session_expired",
 };
 
-export const IS_PROD = (process.env.NODE_ENV ?? "production") === "production";
-export const APP_BASE_URL = process.env.APP_BASE_URL ?? "https://app.superflex.ai/";
-export const API_BASE_URL = process.env.API_BASE_URL ?? "https://api.superflex.ai/v1";
 export const FIGMA_OAUTH_CLIENT_ID = "ZnoCj0vPEwDoLR0jimrfMX";
 export const FIGMA_OAUTH_CALLBACK_URL = `${API_BASE_URL}/auth/figma-callback`;
 
@@ -18,8 +19,6 @@ export const BRAND_NAME = "superflex";
 
 export const AUTH_PROVIDER_ID = `${BRAND_NAME}.auth`;
 export const FIGMA_AUTH_PROVIDER_ID = `${AUTH_PROVIDER_ID}.figma`;
-
-export const SUPERFLEX_POSTHOG_API_KEY = "phc_IHjFcEzOyL1UQAOwifm55y0YgMr1Zd5AQwx8RJC6jgq";
 
 // file extensions that are supported by the extension
 export const SUPPORTED_FILE_EXTENSIONS = [".htm", ".html", ".css", ".scss", ".js", ".ts", ".jsx", ".tsx", ".vue"];
--- webview-ui/src/index.tsx
+++ webview-ui/src/index.tsx
@@ -1,11 +1,17 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import posthog from 'posthog-js';
 
 import './index.css';
 import App from './App';
 import store from './core/store';
 import { getVSCodeAPI } from './api/vscodeApi';
+import { IS_PROD, SUPERFLEX_POSTHOG_API_KEY } from '../../shared/common/constants';
+
+if (IS_PROD && SUPERFLEX_POSTHOG_API_KEY) {
+  posthog.init(SUPERFLEX_POSTHOG_API_KEY, { api_host: 'https://app.posthog.com' });
+}
 
 const Root = (): JSX.Element => (
   <StrictMode>
