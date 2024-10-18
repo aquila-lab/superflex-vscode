1efdba1382325152a6ebd973cf7e0d8d6c76725d
Boris Jankovic <bjankovic998@gmail.com>
2024-10-18 12:21:32 -0400

    Changes

--- webview-ui/src/views/LoginView.tsx
+++ webview-ui/src/views/LoginView.tsx
@@ -31,49 +31,55 @@ const LoginView: React.FunctionComponent<LoginViewProps> = ({ vscodeAPI }: Login
 
   if (authUniqueLink) {
     return (
-      <div className="flex flex-col items-center justify-center gap-3 h-full max-w-md px-5 pb-4">
-        <h3 className="text-lg font-bold">If you are not redirected to the webpage, copy this link to your browser:</h3>
-        <p className="text-center text-muted-foreground truncate max-w-72">{authUniqueLink}</p>
-        <Button
-          onClick={() => {
-            navigator.clipboard.writeText(authUniqueLink);
-            vscodeAPI.postMessage(
-              newEventRequest(EventType.SEND_NOTIFICATION, { message: 'Link copied to clipboard' })
-            );
-          }}>
-          Copy Link
-        </Button>
+      <div className="flex flex-col items-center justify-center gap-3 h-full px-5 pb-4">
+        <div className="max-w-md">
+          <h3 className="text-lg font-bold">
+            If you are not redirected to the webpage, copy this link to your browser:
+          </h3>
+          <p className="text-center text-muted-foreground truncate max-w-72">{authUniqueLink}</p>
+          <Button
+            onClick={() => {
+              navigator.clipboard.writeText(authUniqueLink);
+              vscodeAPI.postMessage(
+                newEventRequest(EventType.SEND_NOTIFICATION, { message: 'Link copied to clipboard' })
+              );
+            }}>
+            Copy Link
+          </Button>
 
-        <Button className="mt-4" variant={'link'} onClick={() => setAuthUniqueLink(undefined)}>
-          Return to Login
-        </Button>
+          <Button className="mt-4" variant={'link'} onClick={() => setAuthUniqueLink(undefined)}>
+            Return to Login
+          </Button>
+        </div>
       </div>
     );
   }
 
   return (
-    <div className="flex flex-col justify-center items-center h-full max-w-md px-5 pb-4">
-      <h3 className="text-2xl font-bold mb-2">Welcome to Superflex!</h3>
-      <p className="text-center text-muted-foreground mb-4">Your one-stop solution for all your development needs!</p>
+    <div className="flex flex-col justify-center items-center h-full px-5 pb-4">
+      <div className="max-w-md">
+        <h3 className="text-2xl font-bold mb-2">Welcome to Superflex!</h3>
+        <p className="text-center text-muted-foreground mb-4">Your one-stop solution for all your development needs!</p>
 
-      <Button
-        className="w-full mb-4"
-        onClick={() => {
-          vscodeAPI.postMessage(newEventRequest(EventType.LOGIN_CLICKED));
-          vscodeAPI.postMessage(newEventRequest(EventType.CREATE_AUTH_LINK, { action: 'login' }));
-        }}>
-        Sign In
-      </Button>
+        <Button
+          className="w-full mb-4"
+          onClick={() => {
+            vscodeAPI.postMessage(newEventRequest(EventType.LOGIN_CLICKED));
+            vscodeAPI.postMessage(newEventRequest(EventType.CREATE_AUTH_LINK, { action: 'login' }));
+          }}>
+          Sign In
+        </Button>
 
-      <p className="text-muted-foreground">Don&apos;t have an account?</p>
-      <Button
-        variant={'link'}
-        onClick={() => {
-          vscodeAPI.postMessage(newEventRequest(EventType.CREATE_ACCOUNT_CLICKED));
-          vscodeAPI.postMessage(newEventRequest(EventType.CREATE_AUTH_LINK, { action: 'create_account' }));
-        }}>
-        Create free account
-      </Button>
+        <p className="text-muted-foreground">Don&apos;t have an account?</p>
+        <Button
+          variant={'link'}
+          onClick={() => {
+            vscodeAPI.postMessage(newEventRequest(EventType.CREATE_ACCOUNT_CLICKED));
+            vscodeAPI.postMessage(newEventRequest(EventType.CREATE_AUTH_LINK, { action: 'create_account' }));
+          }}>
+          Create free account
+        </Button>
+      </div>
     </div>
   );
 };
