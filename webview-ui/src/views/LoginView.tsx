f180d17eec55e580491101eeeda9e48eb9411564
Boris Jankovic <bjankovic998@gmail.com>
2024-10-29 16:46:50 -0400

    Better login

--- webview-ui/src/views/LoginView.tsx
+++ webview-ui/src/views/LoginView.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { EventMessage, EventPayloads, EventType, newEventRequest } from '../../../shared/protocol';
 import { VSCodeWrapper } from '../api/vscodeApi';
 import { Button } from '../components/ui/Button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../components/ui/Card';
 
 type LoginViewProps = {
   vscodeAPI: Pick<VSCodeWrapper, 'postMessage' | 'onMessage'>;
@@ -32,56 +33,72 @@ const LoginView: React.FunctionComponent<LoginViewProps> = ({ vscodeAPI }: Login
   if (authUniqueLink) {
     return (
       <div className="flex flex-col items-center justify-center h-full px-5 pb-4">
-        <div className="flex flex-col items-center justify-center gap-3 max-w-md">
-          <h3 className="text-lg font-bold">
-            If you are not redirected to the webpage, copy this link to your browser:
-          </h3>
-          <p className="text-center text-muted-foreground truncate max-w-72">{authUniqueLink}</p>
-          <Button
-            onClick={() => {
-              navigator.clipboard.writeText(authUniqueLink);
-              vscodeAPI.postMessage(
-                newEventRequest(EventType.SEND_NOTIFICATION, { message: 'Link copied to clipboard' })
-              );
-            }}>
-            Copy Link
-          </Button>
+        <Card className="max-w-md my-auto">
+          <CardHeader className="flex justify-center items-center">
+            <CardTitle className="text-lg font-semibold">
+              If you are not redirected to the webpage, copy this link to your browser:
+            </CardTitle>
+          </CardHeader>
 
-          <Button className="mt-4" variant={'link'} onClick={() => setAuthUniqueLink(undefined)}>
-            Return to Login
-          </Button>
-        </div>
+          <CardContent className="flex flex-col items-center justify-center">
+            <CardDescription className="text-center text-muted-foreground truncate max-w-72">
+              {authUniqueLink}
+            </CardDescription>
+
+            <Button
+              className="mt-4"
+              onClick={() => {
+                navigator.clipboard.writeText(authUniqueLink);
+                vscodeAPI.postMessage(
+                  newEventRequest(EventType.SEND_NOTIFICATION, { message: 'Link copied to clipboard' })
+                );
+              }}>
+              Copy Link
+            </Button>
+          </CardContent>
+
+          <CardFooter className="flex justify-center items-center">
+            <Button variant={'link'} onClick={() => setAuthUniqueLink(undefined)}>
+              Return to Login
+            </Button>
+          </CardFooter>
+        </Card>
       </div>
     );
   }
 
   return (
-    <div className="flex flex-col justify-between items-center h-full px-5 pb-4">
-      <div className="flex-1 flex flex-col items-center justify-center max-w-md h-full">
-        <h3 className="text-2xl font-bold mb-2">Welcome to Superflex!</h3>
-        <p className="text-center text-muted-foreground mb-4">Your one-stop solution for all your development needs!</p>
+    <div className="flex flex-col items-center justify-center h-full px-5 pb-4">
+      <Card className="max-w-md my-auto">
+        <CardHeader className="flex justify-center items-center">
+          <img src={window.superflexLogoUri} alt="Superflex Logo" className="size-12" />
+          <CardTitle>Welcome to Superflex!</CardTitle>
+          <CardDescription>Your Frontend Engineering Assistant.</CardDescription>
+        </CardHeader>
 
-        <Button
-          className="w-full mb-4"
-          onClick={() => {
-            vscodeAPI.postMessage(newEventRequest(EventType.LOGIN_CLICKED));
-            vscodeAPI.postMessage(newEventRequest(EventType.CREATE_AUTH_LINK, { action: 'login' }));
-          }}>
-          Sign In
-        </Button>
+        <CardContent className="flex flex-col items-center justify-center">
+          <Button
+            className="w-full"
+            onClick={() => {
+              vscodeAPI.postMessage(newEventRequest(EventType.CREATE_ACCOUNT_CLICKED));
+              vscodeAPI.postMessage(newEventRequest(EventType.CREATE_AUTH_LINK, { action: 'create_account' }));
+            }}>
+            Start for Free
+          </Button>
 
-        <p className="text-muted-foreground">Don&apos;t have an account?</p>
-        <Button
-          variant={'link'}
-          onClick={() => {
-            vscodeAPI.postMessage(newEventRequest(EventType.CREATE_ACCOUNT_CLICKED));
-            vscodeAPI.postMessage(newEventRequest(EventType.CREATE_AUTH_LINK, { action: 'create_account' }));
-          }}>
-          Create free account
-        </Button>
-      </div>
+          <p className="text-muted-foreground mt-4">Already have an account?</p>
+          <Button
+            variant={'link'}
+            onClick={() => {
+              vscodeAPI.postMessage(newEventRequest(EventType.LOGIN_CLICKED));
+              vscodeAPI.postMessage(newEventRequest(EventType.CREATE_AUTH_LINK, { action: 'login' }));
+            }}>
+            Sign In
+          </Button>
+        </CardContent>
+      </Card>
 
-      <div className="flex flex-col items-center gap-3 mb-8">
+      <div className="flex flex-col items-center gap-3 mt-auto mb-8">
         <p className="text-lg font-semibold">Watch our onboarding video:</p>
         <div
           className="w-full cursor-pointer relative"
