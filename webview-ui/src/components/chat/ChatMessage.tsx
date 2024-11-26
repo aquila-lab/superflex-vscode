ddb73bfe33b77f09e7fb0754ba450a61ff057776
Boris Jankovic <bjankovic998@gmail.com>
2024-11-26 16:48:09 +0100

    Changes

--- webview-ui/src/components/chat/ChatMessage.tsx
+++ webview-ui/src/components/chat/ChatMessage.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
 import { Role, MessageType, Message } from '../../../../shared/model';
+import { cn } from '../../common/utils';
+import { useAppSelector } from '../../core/store';
 import { ImagePreview } from '../ui/ImagePreview';
 import { MarkdownRender } from '../ui/MarkdownRender';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/Avatar';
 import FeedbackDialog from './FeedbackDialog';
-import { useAppSelector } from '../../core/store';
 
 declare global {
   interface Window {
@@ -29,11 +30,31 @@ export const ChatMessage: React.FC<ChatMessageProps> = ({ message, handleFeedbac
     });
   }, [user.picture]);
 
+  const renderContent = () => {
+    if (message.content.type === MessageType.Text || message.content.type === MessageType.TextDelta) {
+      return (
+        <MarkdownRender
+          role={message.role}
+          mdString={message.content.type === MessageType.Text ? message.content.text : message.content.value}
+        />
+      );
+    }
+
+    if (message.content.type === MessageType.Image || message.content.type === MessageType.Figma) {
+      return <ImagePreview alt="preview image" className="mt-2" src={message.content.image as string} />;
+    }
+
+    return null;
+  };
+
   const showFeedback = message.role === Role.Assistant && !message.feedback;
 
   return (
     <div
-      className={`py-4 px-2 border-b border-border text-left rounded-lg ${message.role === Role.User ? 'bg-muted' : undefined}`}>
+      className={cn(
+        'py-4 px-2 border-b border-border text-left rounded-lg',
+        message.role === Role.User ? 'bg-muted' : undefined
+      )}>
       <div className="flex items-center mb-2">
         {message.role !== Role.User && (
           <Avatar className="mr-2 size-5">
@@ -43,31 +64,18 @@ export const ChatMessage: React.FC<ChatMessageProps> = ({ message, handleFeedbac
         )}
         {message.role === Role.User && userInfo.picture && (
           <Avatar className="mr-2 size-5">
-            <AvatarImage src={userInfo.picture} alt="Superflex Logo" />
+            <AvatarImage src={userInfo.picture} alt="User Avatar" />
             <AvatarFallback>{user.username.charAt(0).toUpperCase()}</AvatarFallback>
           </Avatar>
         )}
         <p className="text-sm font-medium text-primary">{message.role === Role.User ? user.username : 'Superflex'}</p>
       </div>
 
-      {message.content.type === MessageType.Text && (
-        <MarkdownRender role={message.role} mdString={message.content.text} />
-      )}
-      {message.content.type === MessageType.TextDelta && (
-        <MarkdownRender role={message.role} mdString={message.content.value} />
-      )}
-
-      {(message.content.type === MessageType.Image || message.content.type === MessageType.Figma) && (
-        <ImagePreview alt="preview image" className="mt-2" src={message.content.image as string} />
-      )}
+      {renderContent()}
 
       {showFeedback && (
         <div className="mt-4">
-          <FeedbackDialog
-            onFeedback={(feedback) => {
-              handleFeedback(message, feedback);
-            }}
-          />
+          <FeedbackDialog onFeedback={(feedback) => handleFeedback(message, feedback)} />
         </div>
       )}
     </div>
