d22d1d5786d86742187d9b90487bd8404636c836
Boris Jankovic <bjankovic998@gmail.com>
2024-11-28 10:59:08 +0100

    Implement auto scroll on bottom

--- webview-ui/src/components/chat/ChatMessageList.tsx
+++ webview-ui/src/components/chat/ChatMessageList.tsx
@@ -1,35 +1,38 @@
-import React, { useRef } from 'react';
+import React from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
+
+import { Message } from '../../../../shared/model';
 import { useAppSelector } from '../../core/store';
 import { LoadingDots } from '../ui/LoadingDots';
 import { ChatMessage } from './ChatMessage';
-import { Message } from '../../../../shared/model';
 
 interface ChatMessageListProps {
   handleMessageFeedback: (message: Message, feedback: string) => void;
 }
 
 export const ChatMessageList: React.FC<ChatMessageListProps> = ({ handleMessageFeedback }) => {
-  const listRef = useRef<HTMLUListElement>(null);
-
   const messages = useAppSelector((state) => state.chat.messages);
   const isMessageStreaming = useAppSelector((state) => state.chat.isMessageStreaming);
   const isMessageProcessing = useAppSelector((state) => state.chat.isMessageProcessing);
 
   return (
     <div className="flex-1 flex flex-col gap-y-2 mb-4 -mt-4 overflow-hidden">
-      <ul ref={listRef} className="flex flex-col gap-y-2 overflow-y-auto">
+      <ScrollToBottom
+        className="flex flex-col gap-y-2 overflow-y-auto"
+        initialScrollBehavior="smooth"
+        followButtonClassName="!bg-muted/50 hover:!bg-muted/80">
         {messages.map((message) => (
-          <li key={message.id}>
+          <div key={message.id}>
             <ChatMessage message={message} handleFeedback={handleMessageFeedback} />
-          </li>
+          </div>
         ))}
 
         {isMessageStreaming && (
-          <li>
+          <div>
             <ChatMessage isStreaming={true} handleFeedback={handleMessageFeedback} />
-          </li>
+          </div>
         )}
-      </ul>
+      </ScrollToBottom>
 
       {isMessageProcessing && !isMessageStreaming && (
         <div className="flex-1 mt-3 ml-2">
