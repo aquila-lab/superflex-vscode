53b447da07e7065fc2badd9470bd8c80e244176f
iivic17 <ilijaivic17@gmail.com>
2025-01-21 17:10:15 +0100

    Handle current file

--- webview-ui-2/src/context/FilesProvider.tsx
+++ webview-ui-2/src/context/FilesProvider.tsx
@@ -1,6 +1,7 @@
-import { createContext, Dispatch, ReactNode, useCallback, useContext, useMemo, useState } from 'react';
-import { EventRequestType, FilePayload } from '../../../shared/protocol';
+import { createContext, Dispatch, ReactNode, useCallback, useContext, useMemo, useRef, useState } from 'react';
+import { EventRequestType, EventResponsePayload, EventResponseType, FilePayload } from '../../../shared/protocol';
 import { usePostMessage } from '../hooks/usePostMessage';
+import { useConsumeMessage } from '../hooks/useConsumeMessage';
 
 interface FilesContextValue {
   selectedFiles: FilePayload[];
@@ -16,11 +17,22 @@ const FilesContext = createContext<FilesContextValue | null>(null);
 
 export const FilesProvider = ({ children }: { children: ReactNode }) => {
   const postMessage = usePostMessage();
-  const [selectedFiles, setSelectedFiles] = useState<FilePayload[]>([]);
+  const [manuallySelectedFiles, setManuallySelectedFiles] = useState<FilePayload[]>([]);
   const [previewedFile, setPreviewedFile] = useState<FilePayload | null>(null);
+  const [currentFile, setCurrentFile] = useState<FilePayload | null>(null);
+
+  const selectedFiles = useMemo(() => {
+    const files = manuallySelectedFiles.map((file) => (file.id === currentFile?.id ? currentFile : file));
+
+    if (currentFile && !files.some((file) => file.id === currentFile.id)) {
+      files.push(currentFile);
+    }
+
+    return files;
+  }, [manuallySelectedFiles, currentFile]);
 
   const selectFile = useCallback((file: FilePayload) => {
-    setSelectedFiles((prevSelectedFiles) => {
+    setManuallySelectedFiles((prevSelectedFiles) => {
       const isSelected = prevSelectedFiles.some((selected) => selected.id === file.id);
       if (isSelected) {
         setPreviewedFile((curr) => (curr?.id === file.id ? null : curr));
@@ -31,8 +43,14 @@ export const FilesProvider = ({ children }: { children: ReactNode }) => {
     });
   }, []);
 
+  const handleNewOpenFile = useCallback((payload: EventResponsePayload[EventResponseType.SET_CURRENT_OPEN_FILE]) => {
+    setCurrentFile(payload);
+  }, []);
+
+  useConsumeMessage(EventResponseType.SET_CURRENT_OPEN_FILE, handleNewOpenFile);
+
   const deselectFile = useCallback((file: FilePayload) => {
-    setSelectedFiles((prevSelectedFiles) => prevSelectedFiles.filter((selected) => selected.id !== file.id));
+    setManuallySelectedFiles((prevSelectedFiles) => prevSelectedFiles.filter((selected) => selected.id !== file.id));
     setPreviewedFile((curr) => (curr?.id === file.id ? null : curr));
   }, []);
 
@@ -48,7 +66,15 @@ export const FilesProvider = ({ children }: { children: ReactNode }) => {
   );
 
   const value: FilesContextValue = useMemo(
-    () => ({ selectedFiles, previewedFile, fetchFiles, fetchFileContent, selectFile, deselectFile, setPreviewedFile }),
+    () => ({
+      selectedFiles,
+      previewedFile,
+      fetchFiles,
+      fetchFileContent,
+      selectFile,
+      deselectFile,
+      setPreviewedFile
+    }),
     [selectedFiles, previewedFile, fetchFiles, fetchFileContent, selectFile, deselectFile, setPreviewedFile]
   );
 
