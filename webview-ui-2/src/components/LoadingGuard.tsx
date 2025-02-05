331bc21cc344571a4a8329f182c7da46efa091c5
iivic17 <ilijaivic17@gmail.com>
2025-02-05 19:55:14 +0100

    Cover user subscription in loading guard

--- webview-ui-2/src/components/LoadingGuard.tsx
+++ webview-ui-2/src/components/LoadingGuard.tsx
@@ -2,9 +2,9 @@ import type { ReactNode } from 'react'
 import { useUser } from '../context/UserContext'
 
 export const LoadingGuard = ({ children }: { children: ReactNode }) => {
-  const { isUserLoading } = useUser()
+  const { isUserLoading, isSubscriptionLoading } = useUser()
 
-  if (isUserLoading) {
+  if (isUserLoading || isSubscriptionLoading) {
     return null
   }
 
