5b78d991894fd2ed551826d49168e19f0247e4c2
iivic17 <ilijaivic17@gmail.com>
2025-02-08 14:18:55 +0100

    Fix apply state

--- webview-ui-2/src/pages/chat/ApplyControls.tsx
+++ webview-ui-2/src/pages/chat/ApplyControls.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import type { ApplyState } from '../../common/utils'
 import { ApplySpinner } from './ApplySpinner'
 import { ApplyButton } from './ApplyButton'
@@ -16,24 +16,30 @@ export const ApplyControls = ({
 }: { filePath: string; content: string }) => {
   const postMessage = usePostMessage()
   const [applyState, setApplyState] = useState<ApplyState>('idle')
+  const isAwaiting = useRef(false)
 
   const handleApply = useCallback(() => {
     postMessage(EventRequestType.FAST_APPLY, { filePath, edits: content })
     setApplyState('applying')
+    isAwaiting.current = true
   }, [postMessage, content, filePath])
 
   const handleAccept = useCallback(() => {
     postMessage(EventRequestType.FAST_APPLY_ACCEPT, { filePath })
     setApplyState('idle')
+    isAwaiting.current = false
   }, [postMessage, filePath])
 
   const handleReject = useCallback(() => {
     postMessage(EventRequestType.FAST_APPLY_REJECT, { filePath })
     setApplyState('idle')
+    isAwaiting.current = false
   }, [postMessage, filePath])
 
   const handleApplyResponse = useCallback(() => {
-    setApplyState('applied')
+    if (isAwaiting.current) {
+      setApplyState('applied')
+    }
   }, [])
 
   useConsumeMessage(EventResponseType.FAST_APPLY, handleApplyResponse)
