9cdc3c3e0e21292d3a240f430656eeb2d8d2b07c
iivic17 <ilijaivic17@gmail.com>
2025-02-08 14:40:39 +0100

    Click on file name opens file

--- webview-ui-2/src/pages/chat/FileInfo.tsx
+++ webview-ui-2/src/pages/chat/FileInfo.tsx
@@ -6,21 +6,36 @@ import {
 } from '@radix-ui/react-tooltip'
 import { getFileName } from '../../common/utils'
 import { FileIcon } from '../../components/ui/FileIcon'
+import { usePostMessage } from '../../hooks/usePostMessage'
+import { EventRequestType } from '../../../../shared/protocol'
+import { useCallback } from 'react'
 
-export const FileInfo = ({ filePath }: { filePath: string }) => (
-  <div className='flex items-center gap-1 min-w-0'>
-    <FileIcon filePath={filePath} className='size-4' />
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <p className='text-[11px] text-foreground truncate max-w-full overflow-hidden whitespace-nowrap text-overflow-ellipsis m-0 cursor-pointer'>
-            {getFileName(filePath)}
-          </p>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p className='text-xs m-0 text-muted-foreground'>{filePath}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
-  </div>
-)
+export const FileInfo = ({ filePath }: { filePath: string }) => {
+  const postMessage = usePostMessage()
+
+  const handleFileNameClick = useCallback(
+    () => postMessage(EventRequestType.OPEN_FILE, { filePath }),
+    [postMessage, filePath]
+  )
+
+  return (
+    <div className='flex items-center gap-1 min-w-0'>
+      <FileIcon filePath={filePath} className='size-4' />
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <p
+              onClick={handleFileNameClick}
+              className='text-[11px] text-foreground truncate max-w-full overflow-hidden whitespace-nowrap text-overflow-ellipsis m-0 cursor-pointer'
+            >
+              {getFileName(filePath)}
+            </p>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p className='text-xs m-0 text-muted-foreground'>{filePath}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  )
+}
