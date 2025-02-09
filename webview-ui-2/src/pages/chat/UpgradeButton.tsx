946be70f6158de2f545c9960ae974fde359a9599
iivic17 <ilijaivic17@gmail.com>
2025-02-09 17:39:01 +0100

    Open external url on upgrade button

--- webview-ui-2/src/pages/chat/UpgradeButton.tsx
+++ webview-ui-2/src/pages/chat/UpgradeButton.tsx
@@ -1,11 +1,18 @@
 import { useCallback } from 'react'
 import { Button } from '../../components/ui/Button'
 import { useUser } from '../../context/UserContext'
+import { usePostMessage } from '../../hooks/usePostMessage'
+import { EventRequestType } from '../../../../shared/protocol'
 
 export const UpgradeButton = () => {
   const { subscription } = useUser()
+  const postMessage = usePostMessage()
 
-  const handleSubscribe = useCallback(() => {}, [])
+  const handleSubscribe = useCallback(() => {
+    postMessage(EventRequestType.OPEN_EXTERNAL_URL, {
+      url: 'https://app.superflex.ai/pricing'
+    })
+  }, [postMessage])
 
   if (!subscription.plan?.name.toLowerCase().includes('free')) {
     return null
