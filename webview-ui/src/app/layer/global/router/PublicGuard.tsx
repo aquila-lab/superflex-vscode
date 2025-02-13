4688d4dcb2814c73c482e660d459e7633e09d048
iivic17 <ilijaivic17@gmail.com>
2025-02-13 22:55:47 +0100

    Loader changes

--- webview-ui/src/app/layer/global/router/PublicGuard.tsx
+++ webview-ui/src/app/layer/global/router/PublicGuard.tsx
@@ -1,17 +1,18 @@
 import { Outlet, useNavigate } from 'react-router-dom'
 import { useGlobal } from '../GlobalProvider'
+import { LoadingView } from '../../../view/loading/LoadingView'
 
 export const PublicGuard = () => {
   const { isLoggedIn } = useGlobal()
   const navigate = useNavigate()
 
   if (isLoggedIn === null) {
-    return null
+    return <LoadingView />
   }
 
   if (isLoggedIn) {
     navigate('/chat', { replace: true })
-    return null
+    return <LoadingView />
   }
 
   return <Outlet />
