61acf14bab1cd69f2d6ce1a07e35c3b367fe298e
Dusan Radivojevic <dusan@superflex.ai>
2025-03-03 13:21:56 +0100

    formatting

--- src/api/promptEnhancer.ts
+++ src/api/promptEnhancer.ts
@@ -1,38 +1,38 @@
-import { MessageContent } from 'shared/model';
+import type { MessageContent } from '../../shared/model'
 import { Api } from './api'
 import { parseError } from './error'
 
-async function enhancePrompt(messageContent: MessageContent): Promise<MessageContent> {
+async function enhancePrompt(
+  messageContent: MessageContent
+): Promise<MessageContent> {
   try {
     if (!messageContent.attachment) {
-      return messageContent;
+      return messageContent
     }
 
-    let image;
+    let image: string
     if (messageContent.attachment.image) {
-      image = messageContent.attachment.image;
-    }
-    else if (messageContent.attachment.figma) {
+      image = messageContent.attachment.image
+    } else if (messageContent.attachment.figma) {
       if (!messageContent.attachment.figma.imageUrl) {
-        console.warn("No image URL for Figma attachment")
-        return messageContent;
+        console.warn('No image URL for Figma attachment')
+        return messageContent
       }
-      image = messageContent.attachment.figma.imageUrl;
-    }
-    else {
-      console.warn("Unsupported attachment type")
-      return messageContent;
+      image = messageContent.attachment.figma.imageUrl
+    } else {
+      console.warn('Unsupported attachment type')
+      return messageContent
     }
 
-    const text = messageContent.text ?? "";
+    const text = messageContent.text ?? ''
 
-    const { data } = await Api.post('/prompt/enhance', { text, image });
+    const { data } = await Api.post('/prompt/enhance', { text, image })
 
     return Promise.resolve({
       ...messageContent,
       originalText: messageContent.text,
       text: data.text
-    });
+    })
   } catch (err) {
     return Promise.reject(parseError(err))
   }
