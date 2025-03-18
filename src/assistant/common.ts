16a40dc50f910af5ec394275a78dbb5f3947750a
Dusan Radivojevic <dusan@superflex.ai>
2025-03-18 23:39:06 +0100

    FigmaService created, validating URLs in chat

--- src/assistant/common.ts
+++ src/assistant/common.ts
@@ -2,3 +2,27 @@
 export function createFilesMapName(provider: string, version: number): string {
   return `${provider}-files-map-v${version}.json`.toLocaleLowerCase()
 }
+
+// Validates user's prompt before sending it to the backend
+export function validateInputMessage(message: string | undefined): string {
+  if (!message)
+    return "";
+
+  if (containsFigmaURL(message))
+    return "Figma URLs are not supported within the text input, please use designated modal for that."
+
+  if (containsURL(message))
+    return "URLs are not yet supported within the text input, please update your message and try again."
+
+  return "";
+}
+
+function containsFigmaURL(inputString: string): boolean {
+    const figmaURLPattern = /https?:\/\/(www\.)?figma\.com\//;    
+    return figmaURLPattern.test(inputString);
+}
+
+function containsURL(inputString: string): boolean {
+    const urlPattern = /https?:\/\/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(\S*)?/;
+    return urlPattern.test(inputString);
+}
---
+++ src/services/FigmaService.ts
@@ -0,0 +1,132 @@
+import { Node, FileNodesResponse } from "figma-js";
+import {
+    FigmaValidationErrorType,
+    FigmaValidationResult,
+} from "shared/model/Figma.model";
+import { parseFigmaApiError } from "src/api/error";
+
+function createFigmaValidationResult(
+    severity: "error" | "warning" | "success",
+    message: string,
+    errorType?: FigmaValidationErrorType,
+    data?: any
+): FigmaValidationResult {
+    return {
+        severity,
+        message,
+        ...(errorType && { errorType }),
+        ...(data && { data }),
+    };
+}
+
+function createFigmaValidationError(
+    message: string,
+    errorType?: FigmaValidationErrorType,
+    data?: any
+): FigmaValidationResult {
+    return createFigmaValidationResult("error", message, errorType, data);
+}
+
+function createFigmaValidationWarning(
+    message: string,
+    errorType?: FigmaValidationErrorType,
+    data?: any
+): FigmaValidationResult {
+    return createFigmaValidationResult("warning", message, errorType, data);
+}
+
+function createFigmaValidationSuccess(
+    message?: string,
+    data?: any
+): FigmaValidationResult {
+    return createFigmaValidationResult("success", message || "", data);
+}
+
+export class FigmaService {
+    static extractSelectionUrlFromResponse(data: any, nodeID: string): string {
+        return data.images[nodeID.replace("-", ":")] ?? data.images[nodeID];
+    }
+
+    static validateFigmaSelection(data: FileNodesResponse, nodeID: string): FigmaValidationResult {
+        const nodeData = data.nodes[nodeID.replace("-", ":")];
+
+        const document = nodeData?.document;
+        // Making sure the selection is not too big, need a more precise check in future
+        if (!document || (document.type as string) === "SECTION")
+            return createFigmaValidationError(
+                "Selection is not supported, please select a frame."
+            );
+
+        const frames = this._extractFrames(document);
+        if (frames.length === 0)
+            return createFigmaValidationError(
+                "No frames found in the selection, please update and try again."
+            );
+
+        const numberOfFrames = frames.length;
+        const framesWithoutAutoLayout = frames.filter(
+            (frame) => !this.hasAutoLayout(frame)
+        );
+        const framesWithAutoLayout =
+            numberOfFrames - framesWithoutAutoLayout.length;
+
+        // If there are more than 50% of frames without auto layout, we don't proceed with the code generation
+        if (framesWithAutoLayout < framesWithoutAutoLayout.length)
+            return createFigmaValidationError(
+                "There are too many frames without auto layout in the given selection, please update and try again.",
+                FigmaValidationErrorType.TooManyAbsoluteFrames,
+                {
+                    framesWithoutAutoLayout: framesWithoutAutoLayout.map(
+                        (frame) => frame.name
+                    ),
+                }
+            );
+
+        if (framesWithAutoLayout < numberOfFrames)
+            return createFigmaValidationWarning(
+                "There are frames without auto layout and generated code may not be pixel-perfect. Would you still like to continue?",
+                FigmaValidationErrorType.SomeAbsoluteFrames,
+                {
+                    framesWithoutAutoLayout: framesWithoutAutoLayout.map(
+                        (frame) => frame.name
+                    ),
+                }
+            );
+
+        return createFigmaValidationSuccess();
+    }
+
+    private static _extractFrames(node: Node, frames: Node[] = []): Node[] {
+        // If the current node is a 'Frame', add it to the results.
+        if (node.type === "FRAME") {
+            frames.push(node);
+        }
+
+        // If the node has children, recursively search each child.
+        if (
+            "children" in node &&
+            Array.isArray(node.children) &&
+            node.children.length > 0
+        ) {
+            node.children.forEach((child) => this._extractFrames(child, frames));
+        }
+
+        return frames;
+    }
+
+    /**
+     * Checks if a node has auto layout.
+     * @param node - The Figma node to check.
+     * @returns True if the node has auto layout.
+     */
+    static hasAutoLayout(node: Node): boolean {
+        if ("layoutPositioning" in node &&
+            node.layoutPositioning === "ABSOLUTE")
+            return false;
+
+        return (
+            "layoutMode" in node &&
+            (node.layoutMode === "HORIZONTAL" || node.layoutMode === "VERTICAL")            
+        );
+    }
+}
