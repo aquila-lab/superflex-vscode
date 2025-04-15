d34953bff6bbcca1db1d8e9a639a716814507642
iivic17 <ilija@superflex.ai>
2025-04-15 17:30:23 +0200

    Refactor MessageListContainer component for improved layout
    - Adjusted margin-bottom in MessageListContainer to enhance spacing consistency.
    - Maintained the overall structure and functionality of the component.
    
    These changes improve the visual layout of the message list, contributing to a more organized chat interface.

--- webview-ui/src/app/views/chat/components/message/MessageListContainer.tsx
+++ webview-ui/src/app/views/chat/components/message/MessageListContainer.tsx
@@ -3,7 +3,7 @@ import ScrollToBottom from 'react-scroll-to-bottom'
 
 export const MessageListContainer = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='flex-1 flex flex-col gap-y-2 mb-4 overflow-hidden'>
+    <div className='flex-1 flex flex-col gap-y-2 mb-2 overflow-hidden'>
       <ScrollToBottom
         className='flex flex-col gap-y-2 overflow-y-auto scroll-to-bottom'
         initialScrollBehavior='auto'
