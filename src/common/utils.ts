316171cc7c4a400f64338ab11291df7790940d80
Dusan Radivojevic <dusan@superflex.ai>
2025-04-23 16:55:06 +0100

    Excluding opacity (#74)

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -148,12 +148,10 @@ export function rgbaToHex(color: {
   r: number
   g: number
   b: number
-  a: number
 }): string {
   const r = Math.round(color.r * 255)
   const g = Math.round(color.g * 255)
   const b = Math.round(color.b * 255)
-  const opacity = Math.round(color.a * 100)
 
-  return `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}${opacity.toString(16).padStart(2, '0')}`
+  return `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`
 }
